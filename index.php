7bd3b7e9b0fc56eb5f84ff35e68b0fe88c9463e4
Jarad DeLorenzo <jaradd@gmail.com>
2011-05-10 19:54:49 -0400

    HTML5ing it!

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
   <head>
     <title>Jarad DeLorenzo</title>
     <link type="text/css" rel="stylesheet" href="css/reset.css" />
-<!--    <link href='http://fonts.googleapis.com/css?family=Cabin+Sketch:bold' rel='stylesheet' type='text/css' />
-    <link href='http://fonts.googleapis.com/css?family=Luckiest+Guy' rel='stylesheet' type='text/css' />
-    <link href='http://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css' />-->
     <link type="text/css" rel="stylesheet" href="css/home.css" />    
     <script type="text/javascript" src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/canvas.js"></script>
   </head>
   <body>
     <div id="content">
-    </div>
-    <div id="footer">
-<!--        <ul>
-        <li>jaradd@gmail.com</li>
-        <li>973.440.8809</li>
-        <li><a href="#">Resume</a></li>
-        <li><a href="http://www.linkedin.com/profile/view?id=12114139">LinkedIn</a></li>
-        <li><a href="http://www.facebook.com/jaradd">Facebook</a></li>
-      </ul>-->
-      <span>Copyright &copy; 2011 somethin somethin somethin</span>
+      <canvas id="clouds" width="1024" height="900"></canvas>
+      <div id="footer">
+        <span>Copyright &copy; 2011 somethin somethin somethin</span>
+      </div>      
     </div>
   </body>
 </html>
---
+++ js/canvas.js
@@ -0,0 +1,10 @@
+function draw(){
+  var canvas = document.getElementById('clouds');
+  if (canvas.getContext){
+    var ctx = canvas.getContext('2d');
+  }
+}
+
+$(document).ready(function() {
+  
+});
