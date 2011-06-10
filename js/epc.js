e4ada01fc6bb79be58b093f61883127de00540c9
unknown <delorenj@.(none)>
2011-06-10 19:06:42 -0400

    Added a sweet new box2d framework that seems more complete

--- js/epc.js
+++ js/epc.js
@@ -228,23 +228,24 @@ var EPC = (function () {
       
       getBodyAtMouse : function(e) {
         var aabb = new b2AABB();
+        
         var mousePVec = new b2Vec2();
         aabb.minVertex.Set(e.pageX - 0.001, e.pageY - 0.001);
         aabb.maxVertex.Set(e.pageX + 0.001, e.pageY + 0.001);
         mousePVec.Set(e.pageX, e.pageY);
+        console.log("(" + e.pageX + ", " + e.pageY +")");
         for (var b = world.m_bodyList; b; b = b.m_next) {
           for (var s = b.GetShapeList(); s != null; s = s.GetNext()) {
             if(!s.GetBody().IsStatic()){
-              var inside = s.TestPoint(s.GetBody().GetTransform(), mousePVec);
+              var inside = s.TestPoint(mousePVec);
               if(inside) {
                 console.log("got shape!");
                 break;
               }
             }
           }
-        }        
+        }     
       }
-      
     }
   })();
   
@@ -354,6 +355,48 @@ jQuery(document).ready(function() {
   EPC.initLinkClouds();
   EPC.initBgClouds();
   EPC.initBird();
-  EPC.initCanvas();
-   
+  //EPC.initCanvas();
+(function(){
+
+var Test = function() {
+    b2jsTest.__constructor.apply(this, arguments);
+};
+extend(Test.prototype, b2jsTest.prototype)
+
+Test.prototype.createWorld = function() {
+    var world = b2jsTest.prototype.createWorld.apply(this, arguments);
+    world.DestroyBody(this._wallTop);
+    var boxsize = 1.4;
+    function spawn(x, y, a) {
+        var bodyDef = new b2BodyDef();
+        bodyDef.type = b2Body.b2_dynamicBody;
+        bodyDef.position.Set(x, y);
+        bodyDef.angle = a;
+        var body = world.CreateBody(bodyDef);
+        body.w = boxsize;
+        body.h = boxsize;
+        var shape = new b2PolygonShape.AsBox(body.w, body.h);
+        var fixtureDef = new b2FixtureDef();
+        fixtureDef.restitution = 0.0;
+        fixtureDef.density = 2.0;
+        fixtureDef.friction = 0.9;
+        fixtureDef.shape = shape;
+        body.CreateFixture(fixtureDef);
+        return body;
+    }
+
+    for(var i = 0; i < 100; i ++) {
+        spawn(32 + Math.sin(i/5) * (6 * boxsize), 32 + i * (2.8 * boxsize), 0);
+    }
+    
+    return world;
+};
+
+window.epc = Test;
+    
+})();
+var runner = new epc(jQuery("#canvas")[0]);
+runner.draw();	
+runner.resume();
+	
 });
